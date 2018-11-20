6ddd63493deac3ae3f49172ccd73d84b0d2cdb42
Marcin Piwek <marcin.piwek@gmail.com>
2018-11-20 14:06:36 +0100

    message controller

--- web/controllers/message_controller.ex
+++ web/controllers/message_controller.ex
@@ -4,4 +4,28 @@ defmodule GibberChat.MessageController do
     GibberChat.User.auth_adm_helper(conn,token)
   end
   
+  def update(conn, %{"auth_token" => auth_token,
+                     "body" => body,
+                     "options" => opts
+                     }) do
+    adm = auth_adm(conn,auth_token)
+    message = find_message(conn, id)
+    changeset = GibberChat.Message.changeset(message, %{{body: body, options: opts})
+    r = GibberChat.Repo.update(changeset)
+    json(conn, message_response(elem(r,1)))
+  end
+
+  def delete(conn, %{"auth_token" => auth_token, "id" => id}) do
+    adm = auth_adm(conn, auth_token)
+    message = find_message(conn, id)
+    GibberChat.Repo.delete!(message)
+    json(conn, %{status: "deleted"})
+  end
+
+  def message_response(message) do
+      %{id: message.id,
+        body: message.body,
+        options: message.options
+      }
+  end
 end
