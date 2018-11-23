85dae402f10216037f58eca81bbd1be71c7718bc
Marcin Piwek <marcin.piwek@gmail.com>
2018-11-23 09:38:39 +0100

    user auth

--- web/models/room_user.ex
+++ web/models/room_user.ex
@@ -17,4 +17,19 @@ defmodule GibberChat.RoomUser do
     |> cast(params, [:auth_token])
     |> validate_required([:auth_token])
   end
+
+  def auth_user(room_id, user_token) do
+    query = from ru in GibberChat.RoomUser, where: ru.room_id == ^room_id and ru.auth_token == ^user_token
+    res = GibberChat.Repo.all(query)
+    s = "none"
+    IO.puts "is empty"
+    IO.inspect Enum.empty?(res)
+    unless Enum.empty?(res) do 
+      IO.puts "OK"
+      %{status: "ok", res: res}
+    else
+      %{status: "none", res: res}
+    end
+
+  end
 end
